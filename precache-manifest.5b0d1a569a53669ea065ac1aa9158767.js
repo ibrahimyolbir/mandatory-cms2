20b1b558e0d21893d96ddbe7ffc44fd2431c453c
ibrahim yolbir <ibrahimyolbirr@gmail.com>
2019-10-08 10:46:55 +0200

    Updates

---
+++ precache-manifest.5b0d1a569a53669ea065ac1aa9158767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67761fef2c41c6d4d590c18e17a6061a",
+    "url": "/mandatory-cms2/index.html"
+  },
+  {
+    "revision": "d936b687560af271ab76",
+    "url": "/mandatory-cms2/static/css/2.a00eef2d.chunk.css"
+  },
+  {
+    "revision": "719b1787c6201d159e26",
+    "url": "/mandatory-cms2/static/css/main.35bde46c.chunk.css"
+  },
+  {
+    "revision": "d936b687560af271ab76",
+    "url": "/mandatory-cms2/static/js/2.3b3a191c.chunk.js"
+  },
+  {
+    "revision": "719b1787c6201d159e26",
+    "url": "/mandatory-cms2/static/js/main.6ff9807b.chunk.js"
+  },
+  {
+    "revision": "b0d19943796fcb15d08a",
+    "url": "/mandatory-cms2/static/js/runtime~main.3f9fd334.js"
+  }
+]);
